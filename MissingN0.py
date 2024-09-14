624a45f9109754c912dc279c079ea9a66023f390
Reuben Holmes <104584906@student.swin.edu.au>
2024-09-14 17:51:03 +1000

    fixed backslashes

--- MissingN0.py
+++ MissingN0.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load dataset
-file_path = "COS30049-Assignment-2\Raw Datasets\house-prices-by-small-area-sale-year.csv"
+file_path = "COS30049-Assignment-2/Raw Datasets/house-prices-by-small-area-sale-year.csv"
 sales_data = pd.read_csv(file_path)
 
 # identifying the missing data
@@ -11,7 +11,7 @@ print("\n\n" + file_path + "\n")
 print(missing_values)
 
 # Load dataset
-file_path = "COS30049-Assignment-2\Raw Datasets\MELBOURNE_HOUSE_PRICES_LESS.csv"
+file_path = "COS30049-Assignment-2/Raw Datasets/MELBOURNE_HOUSE_PRICES_LESS.csv"
 sales_data = pd.read_csv(file_path)
 
 # identifying the missing data
@@ -21,7 +21,7 @@ print("\n\n" + file_path + "\n")
 print(missing_values)
 
 # Load dataset
-file_path = "COS30049-Assignment-2\Raw Datasets\Melbourne_housing_FULL_acedit.csv"
+file_path = "COS30049-Assignment-2/Raw Datasets/Melbourne_housing_FULL_acedit.csv"
 sales_data = pd.read_csv(file_path)
 
 # identifying the missing data
@@ -31,7 +31,7 @@ print("\n\n" + file_path + "\n")
 print(missing_values)
 
 # Load dataset
-file_path = "COS30049-Assignment-2\Raw Datasets\Melbourne_housing_FULL.csv"
+file_path = "COS30049-Assignment-2/Raw Datasets/Melbourne_housing_FULL.csv"
 sales_data = pd.read_csv(file_path)
 
 # identifying the missing data
@@ -41,7 +41,7 @@ print("\n\n" + file_path + "\n")
 print(missing_values)
 
 # Load dataset
-file_path = "COS30049-Assignment-2\Raw Datasets\Melbourne_housing.csv"
+file_path = "COS30049-Assignment-2/Raw Datasets/Melbourne_housing.csv"
 sales_data = pd.read_csv(file_path)
 
 # identifying the missing data
