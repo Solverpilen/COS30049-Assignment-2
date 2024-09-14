2aa6c405c20c6b5e6995a93f47d7aca420c0de3f
Reuben Holmes <104584906@student.swin.edu.au>
2024-09-14 17:49:37 +1000

    Code to show missing values

--- MissingN0.py
+++ MissingN0.py
@@ -1,10 +1,51 @@
 import pandas as pd
 
 # Load dataset
-file_path = "./Raw Datasets/house-prices-by-small-area-sale-year.csv.csv"
+file_path = "COS30049-Assignment-2\Raw Datasets\house-prices-by-small-area-sale-year.csv"
 sales_data = pd.read_csv(file_path)
 
 # identifying the missing data
 missing_values = sales_data.isnull().sum()
 
+print("\n\n" + file_path + "\n")
+print(missing_values)
+
+# Load dataset
+file_path = "COS30049-Assignment-2\Raw Datasets\MELBOURNE_HOUSE_PRICES_LESS.csv"
+sales_data = pd.read_csv(file_path)
+
+# identifying the missing data
+missing_values = sales_data.isnull().sum()
+
+print("\n\n" + file_path + "\n")
+print(missing_values)
+
+# Load dataset
+file_path = "COS30049-Assignment-2\Raw Datasets\Melbourne_housing_FULL_acedit.csv"
+sales_data = pd.read_csv(file_path)
+
+# identifying the missing data
+missing_values = sales_data.isnull().sum()
+
+print("\n\n" + file_path + "\n")
+print(missing_values)
+
+# Load dataset
+file_path = "COS30049-Assignment-2\Raw Datasets\Melbourne_housing_FULL.csv"
+sales_data = pd.read_csv(file_path)
+
+# identifying the missing data
+missing_values = sales_data.isnull().sum()
+
+print("\n\n" + file_path + "\n")
+print(missing_values)
+
+# Load dataset
+file_path = "COS30049-Assignment-2\Raw Datasets\Melbourne_housing.csv"
+sales_data = pd.read_csv(file_path)
+
+# identifying the missing data
+missing_values = sales_data.isnull().sum()
+
+print("\n\n" + file_path + "\n")
 print(missing_values)
