103f706026f9de851aae6d32a730cfab15003162
solverpilen <j.chumachenko@outlook.com>
2024-09-20 19:17:56 +1000

    update to main.py cleaning data

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import pandas as pd
 
-df = pd.read_csv('house-prices-by-small-area-sale-year.csv')
+df1 = pd.read_csv('house-prices-by-small-area-sale-year.csv')
+df2 = pd.read_csv('MELBOURNE_HOUSE_PRICES_LESS.csv')
+df3 = pd.read_csv('Melbourne_housing_FULL_acedit.csv')
+df4 = pd.read_csv('Melbourne_housing_FULL.csv')
+df5 = pd.read_csv('Melbourne_housing.csv')
 
-df_cleaned = df.dropna()
+columns = ['suburb', 'rooms', 'type', 'date', 'price']
+
+df1 = df1[columns]
+df2 = df2[columns]
+df3 = df3[columns]
+df4 = df4[columns]
+df5 = df5[columns]
+
+combined_df = pd.concat([df1, df2, df3, df4, df5], ignore_index=True)
+
+df_cleaned = combined_df.dropna()
+
+print(combined_df)
 
