17b8b200ecb956b0faa281679cc938e03d103c48
solverpilen <j.chumachenko@outlook.com>
2024-10-24 15:01:29 +1100

    added stuff to website

--- housing_enjoyers_website/src/App.js
+++ housing_enjoyers_website/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ResponsiveAppBar from './components/navbar/navbar';
+
 
 function App() {
 	return (
 		<div>
-			<h1>Hello, welcome to my React app!</h1>
-			<p>This is a custom message replacing the default content.</p>
+			<ResponsiveAppBar />
 		</div>
+		
 	);
 }
   
