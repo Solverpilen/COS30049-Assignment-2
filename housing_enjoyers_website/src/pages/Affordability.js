5a02c5641eda03926cade392204ba34c6e1bb69d
solverpilen <j.chumachenko@outlook.com>
2024-11-03 14:37:21 +1100

    fixed some fomratting for the pie chart and text

--- housing_enjoyers_website/src/pages/Affordability.js
+++ housing_enjoyers_website/src/pages/Affordability.js
@@ -25,8 +25,8 @@ function Affordability() {
     }, []); 
 
     const defaultPieChart = createChart("Default Pie Chart", ["High", "Medium", "Low", "Very Low"], 
-        [chartData.high, chartData.medium, chartData.low, chartData["very low"]], ["blue", "green", "orange", "red"], 
-        ["blue", "green", "orange", "red"]);
+        [chartData.high, chartData.medium, chartData.low, chartData["very low"]], ["blue", "green", "orange", "#FF6666"], 
+        ["blue", "green", "orange", "#FF6666"]);
 
     return (
     <div className="App">
@@ -34,42 +34,58 @@ function Affordability() {
 
   
     <h1>React Line Chart with Chart.js</h1>
-    <Container maxWidth="lg" style={{ height: '100vh', display: 'flex' }}>
+    <Container maxWidth="lg" style={{ marginTop: '200px'}} xs={{ display: 'flex' }}>
     {/* Grid Container to center the charts */}
-    <Grid container
-      spacing={2}
-      justifyContent="center"
-      alignItems="center"
-      item xs={6}>
-        <div>
-            {defaultPieChart}
-        </div>
+        <Grid container
+        spacing={4}
+        justifyContent="center">
+
+
+        
+        
+
+        
+        <Grid container item xs={12} spacing={10} justifyContent="center">
+
+            <Grid xs = {12} md = {6} xs={{display: "flex", justifyContent : "center"}}>
+            <div>
+                {defaultPieChart}
+            </div>
+            </Grid>
+            <Grid  container spacing={6}
+        // alignItems="center"
+            item xs={12} md={6} xs={{display: "flex"}}
+            >
+
+                <Box display="flex" 
+                >
+                <Typography align='justify' xs={{border: '2px solid #1976d2' , borderRadius: '16px'}}>The median weekly income of Australia is $1,500 in 2023. If that person works 50 weeks of the year
+                    then that comes to a total of $75,000 per year. Assuming monthly expenses of 1,000 per month, 
+                    this particular person can borrow a maximum of $394,000 to pay for their property. 
+                    This is what the pie chart to the left shows the affordability of such properties from 2017 to 2018 according 
+                    these statistics.
+                </Typography>
+                </Box>
+
+
+            </Grid>
+
+
+
+        
+
+        <Grid display="flex" alignItems="center">
+
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+
+        </Grid>
+
+        </Grid>
+        </Grid>
        
-
-    </Grid>
-
-    <Grid container
-    spacing={6}
-    justifyContent="center"
-   // alignItems="center"
-    item xs={6}>
-
-    <Box display="flex" 
-    justifyContent="center"
-    alignItems="center">
-    <p> The median weekly income of Australia is $1,500 in 2023. If that person works 50 weeks of the year
-        then that comes to a total of $75,000 per year. Assuming monthly expenses of 1,000 per month, 
-        this particular person can borrow a maximum of $394,000 to pay for their property. 
-        This is what the pie chart to the left shows the affordability of such properties from 2017 to 2018 according 
-        these statistics.
-    </p>
-    </Box>
-
-
-    </Grid>
-
-   
     </Container>
+   
+
     </div>
 
 
--- housing_enjoyers_website/src/pages/Home.js
+++ housing_enjoyers_website/src/pages/Home.js
@@ -5,7 +5,6 @@ function Home() {
 
     return (
 
-            
         <div className="background fade_in_image">
          <ResponsiveAppBar />
         <h1 className="fade_in_move title"> Welcome to Housing Enjoyers Predictive Website!</h1>
