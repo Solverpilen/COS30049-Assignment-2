07a8407e4a77b059cf266c643efe6f1d279df934
Reuben Holmes <104584906@student.swin.edu.au>
2024-11-10 20:49:22 +1100

    re-enabling prediction

--- housing_enjoyers_website/src/App.js
+++ housing_enjoyers_website/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Affordability from './pages/Affordability.js';
+import Prediction from './pages/Prediction.js';
 import Home from './pages/Home.js';
 
 
@@ -13,6 +14,7 @@ function App() {
 				<Routes>
 					<Route path='/' element={<Home/>} />
 					<Route path='/Affordability' element={<Affordability/>} />
+					<Route path='/Prediction' element={<Prediction/>} />
 				</Routes>
 			</BrowserRouter>
 		</div>
